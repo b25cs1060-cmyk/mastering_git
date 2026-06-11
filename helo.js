15b88a22cdf911a19c0e1c7ce89af495fa29994b
rudri <b25cs1060@iitj.ac.in>
2026-06-12 02:52:18 +0530

    completed stash work

--- helo.js
+++ helo.js
@@ -2,3 +2,6 @@ console.log("hello")
 console.log("new line added to the js file")
 console.log("i may delete this  later")
 console.log("new change1 ")
+//comment 1
+//making some important chnage into the dodcument
+//but the bugs are still intact
