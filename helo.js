f888e876f5f7f571edbb5f86129f7dfaf485e42d
rudri <b25cs1060@iitj.ac.in>
2026-06-12 01:53:35 +0530

    i might delete this later on

--- helo.js
+++ helo.js
@@ -1,4 +1,3 @@
 console.log("hello")
 console.log("new line added to the js file")
-console.log("adding a line that can destroy the code")
-console.log("bad code")
+console.log("i may delete this  later")
